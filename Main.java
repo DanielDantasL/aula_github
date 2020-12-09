bcfc3bb35442c192b916d94841ae86f194d62a47
Estevam Amorim <estevamyusukeamorim@gmail.com>
2020-12-09 07:55:58 -0300

    Implementa Retirada de Saque #3

--- Main.java
+++ Main.java
@@ -3,7 +3,8 @@ import java.util.Arrays;
 public class Main {
 
 	public static void main(String[] args) {
-		Menu mainMenu =  new Menu("Menu Principal", Arrays.asList("Conta", "Cliente", "Operacoes", "Cadastrar Cliente", "Abrir Conta"));
+		Menu mainMenu =  new Menu("Menu Principal", Arrays.asList("Conta", "Cliente", "Operacoes",
+"Cadastrar Cliente", "Abrir Conta", "Realizar Saque"));
 		System.out.println(mainMenu.getSelection() + "foi selecionada");
 		System.out.println("Fim");
 	}
